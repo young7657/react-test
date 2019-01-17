2ccec8fe51a391c6ea53a24e3544fb4df8dabaee
扬神233 <young129@126.com>
2019-01-17 22:05:20 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,23 +6,45 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      	<Clock />
       </div>
     );
   }
+}
+
+class Clock extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {date: new Date()};
+	}
+
+	componentDidMount() {
+		console.log(111)
+		this.timerID = setInterval(
+			() => this.tick(),
+			1000
+		);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+
+	tick() {
+		console.log(1111212)
+		this.setState({
+			date: new Date()
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Hello, World</h1>
+				<h2>It is {this.state.date.toLocaleTimeString()}</h2>
+			</div>
+		)
+	}
 }
 
 export default App;
